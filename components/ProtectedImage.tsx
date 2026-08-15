c44b838ed1af3e6082d1c3839ce42672d93dcd07
creation <creation2224@gmail.com>
2026-08-16 02:16:32 +0530

    homepage: make uncropped hero image bigger and wider (22rem width, 30rem max height)

--- components/ProtectedImage.tsx
+++ components/ProtectedImage.tsx
@@ -6,7 +6,7 @@ import meImage from "@/assets/me.png";
 export default function ProtectedImage() {
   return (
     <div
-      className="relative mx-auto w-fit overflow-hidden rounded-[6px] bg-white/5 md:mx-0 flex justify-center items-center"
+      className="relative mx-auto w-full max-w-[22rem] overflow-hidden rounded-[6px] bg-white/5 md:mx-0 flex justify-center items-center"
       onContextMenu={(event) => event.preventDefault()}
     >
       <Image
@@ -15,8 +15,8 @@ export default function ProtectedImage() {
         priority
         draggable={false}
         onDragStart={(event) => event.preventDefault()}
-        sizes="(min-width: 1280px) 18rem, (min-width: 1024px) 25vw, 100vw"
-        className="h-auto max-h-[22rem] sm:max-h-[25rem] w-auto select-none object-contain opacity-90 rounded-[6px]"
+        sizes="(min-width: 1280px) 22rem, (min-width: 1024px) 30vw, 100vw"
+        className="h-auto max-h-[30rem] w-full select-none object-contain opacity-90 rounded-[6px]"
       />
       <div className="absolute inset-0" aria-hidden="true" />
     </div>
